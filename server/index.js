ec68f157c22253a30b3f729b0101f5aa5dd2d113
theanlay <45415836+theanlay@users.noreply.github.com>
2020-09-26 21:29:08 +0700

    add more fil

--- server/index.js
+++ server/index.js
@@ -13,4 +13,4 @@ app.use('/api/tasks',tasks);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('server started on port ${port}'));
+app.listen(port, () => console.log(`server started on port ${port}`));
