a72f3c04b5fcc5f34bc8b6fdefea72bbfc20edb0
theanlay <45415836+theanlay@users.noreply.github.com>
2020-09-27 10:20:51 +0700

    add a full project fullstack

--- server/index.js
+++ server/index.js
@@ -11,6 +11,16 @@ app.use(cors());
 const tasks = require('./routes/api/tasks')
 app.use('/api/tasks',tasks);
 
+
+// handle production 
+
+if(process.env.NODE_ENV === 'production' ){
+
+    app.use(express.static(__dirname +'/public/'));
+
+    app.get(/.*/,(req,res=> res.sendFile(__dirname + '/public/index.html')));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
