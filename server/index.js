641f361adbd880d80cdd7ec59c878e154016c1ea
theanlay <45415836+theanlay@users.noreply.github.com>
2020-09-27 16:13:14 +0700

    add more file

--- server/index.js
+++ server/index.js
@@ -9,18 +9,20 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const tasks = require('./routes/api/tasks')
-app.use('/api/tasks',tasks);
+app.use('/api/tasks', tasks);
 
 
 // handle production 
 
-if(process.env.NODE_ENV === 'production' ){
+if (process.env.NODE_ENV === 'production') {
 
-    app.use(express.static(__dirname +'/public/'));
+    app.use(express.static(__dirname + '/public/'));
 
-    app.get(/.*/,(req,res=> res.sendFile(__dirname + '/public/index.html')));
+    app.get(/.*/, (req, res => res.sendFile(__dirname + '/public/index.html')));
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || "5000;";
+
 
 app.listen(port, () => console.log(`server started on port ${port}`));
+app.set("port", PORT);
