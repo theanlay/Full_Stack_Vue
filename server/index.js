35233d11ca49f4269d930f6d35bf3043c737ddd2
theanlay <45415836+theanlay@users.noreply.github.com>
2020-09-28 20:30:06 +0700

    dfasfd

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use(express.static(__dirname + '/public/'));
 
-    app.get(/.*/, (req, res => res.sendFile(__dirname + '/public/index.html')));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 
