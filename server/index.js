93eff5877a7ceb19c9f427878aa625611c9ba467
theanlay <45415836+theanlay@users.noreply.github.com>
2020-09-28 20:25:21 +0700

    dfasf

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
