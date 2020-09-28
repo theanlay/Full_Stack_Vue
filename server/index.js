d7ab4dc940683af2bfd81fec6fc2c26cee2f8f4f
theanlay <45415836+theanlay@users.noreply.github.com>
2020-09-28 20:04:29 +0700

    afdafd

--- server/index.js
+++ server/index.js
@@ -24,6 +24,5 @@ if (process.env.NODE_ENV === 'production') {
 
 
 
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started on port ${port}`));
-app.set("port", PORT);
